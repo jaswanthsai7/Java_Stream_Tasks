a80f76c22d8877ebb93391e08439f85318f4a646
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-14 08:42:13 +0530

    created fields in Student.java class

--- src/main/java/com/github/jaswanth/task1/Student.java
+++ src/main/java/com/github/jaswanth/task1/Student.java
@@ -6,4 +6,12 @@
 package com.github.jaswanth.task1;
 
 public class Student {
+    // store the rollNumber of a student, as a primitive byte type
+    private byte rollNumber;
+    //store the first name of a student, as a reference String type value.
+    private String firstName;
+    //to store the last name of a student, as a reference String type value
+    private String secondName;
+    //store the current year of a student, as a primitive byte type value.
+    private byte year;
 }
