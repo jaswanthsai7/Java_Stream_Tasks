360337ad99738e210f58faf5b9cc0766e13cbbe3
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-14 08:46:21 +0530

    added PortalCredentialsCreator.java class

---
+++ src/main/java/com/github/jaswanth/task1/PortalCredentialsCreator.java
@@ -0,0 +1,9 @@
+/*
+ * Author Name : Jaswanth Sai
+ * Date : 9/14/2022
+ * Created With: INTELLIJ IDEA COMMUNITY EDITION
+ */
+package com.github.jaswanth.task1;
+
+public class PortalCredentialsCreator {
+}
